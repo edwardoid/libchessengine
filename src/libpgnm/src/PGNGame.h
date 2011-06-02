af27cad06fedeb5f01a69f3f5a98886650867784
Edward Sarkisyan <me@edwardsarkisyan.com>
2011-06-02 19:45:53 +0000

    Author: Ed Sarkisyan
    + Added documentation for few classes from libpgnm
    
    
    
    git-svn-id: https://libchessengine.svn.sourceforge.net/svnroot/libchessengine/trunk@57 301c1b5a-37dd-436e-9a41-048cb4d76510

--- src/libpgnm/src/PGNGame.h
+++ src/libpgnm/src/PGNGame.h
@@ -27,30 +27,118 @@
 #include <PGNTagList.h>
 #include <PGNMoveList.h>
 
+/**
+ * @brief global namespace for libpgnm
+ * @namespace pgn
+ */
 namespace pgn
 {
+
+	/**
+	 * @class Game
+	 * @brief Represents information about game.
+	 */
 	class Game 
 	{
 		public:
-		
+			
+			/**
+			 * @brief Constructs game with given tags, moves and result.
+			 * @param tl A Set of tags and values for current game.
+			 * @param ml list of moves for game.
+			 * @param gr Result of a game.
+			 */
 			Game(const TagList &tl, const MoveList &ml, const GameResult &gr);
+
+			/**
+			 * @brief Copy-constructor.
+			 * @param src Source game.
+			 */
 			Game(const Game& src);
 
+			/**
+			 * @brief Destructor.
+			 */
 			virtual ~Game();
 
+			/**
+			 * @brief Copy given game to current.
+			 * @param src Source game.
+			 * @return Reference to changed game.
+			 */
 			Game& operator = (const Game& src);
+
+			/**
+			 * 2 games are equal if is equal their tags, moves, and result.
+			 * @brief Compares 2 games.
+			 * @param src Source game.
+			 * @return true if games are equal.
+			 */
 			bool operator == (const Game& src) const;
+
+			/**
+			 * @brief Negotive version of operator==().
+			 */
 			bool operator != (const Game& src) const;
 		
+			/**
+			 * Returns tags with values associated to the game.
+			 * @brief Returns tags and values.
+			 * @return Set of tags.
+			 */
 			TagList& tags() const;
+
+			/**
+			 * @brief Returns game's move list.
+			 * @return Game moves.
+			 */
 			MoveList& moves() const;
+
+			/**
+			 * @brief Get time when game was/is 
+			 * finished/going.
+			 * @return Value of tag "Date".
+			 */
 			std::string date() const;
+
+			/**
+			 * @brief Get white player's name.
+			 * @return Value of tag "White".
+			 */
 			std::string white() const;
+			
+			/**
+			 * @brief Get black player's name.
+			 * @return Value of tag "Black".
+			 */
 			std::string black() const;
+			
+			/**
+			 * @brief Get game's result.
+			 * @return Value of tag "Result".
+			 */
 			GameResult result() const;
+
+			/**
+			 * @brief Sets pre-game comment.
+			 * @param ct Comment.
+			 */
 			void bindBeforeGameComment(const CommentText &ct);
+
+			/**
+			 * @brief Sets post-game comment.
+			 * @param ct Comment.
+			 */
 			void bindAfterGameComment(const CommentText &ct);
-		
+
+			/**
+			 * @brief Prints game to stream.
+			 * @param os Stream to print.
+			 * @param src Game to print.
+			 *
+			 *	Prints tags by one to line, then prints moves, and then game
+			 *	result.
+			 */
 			friend std::ostream& operator << ( std::ostream& os, const Game& src);
 
 		private:
