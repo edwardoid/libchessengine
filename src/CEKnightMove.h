29f1f664f487d55a053092dded6428cebddcd101
Edward Sarkisyan <me@edwardsarkisyan.com>
2012-01-08 02:19:18 +0000

    Author: Ed Sarkisyan E-mail: edward.sarkisyan@gmail.com
    - Removed wrong doxy-comments
    + Added ignoding doc directory
    
    
    
    git-svn-id: https://libchessengine.svn.sourceforge.net/svnroot/libchessengine/trunk@68 301c1b5a-37dd-436e-9a41-048cb4d76510

--- src/CEKnightMove.h
+++ src/CEKnightMove.h
@@ -15,60 +15,15 @@ namespace ChEngn
 	{
 	public:
 
-        /**
-        * @brief 
-         *
-         * @param ply
-         * @param isWhite
-         */
 		KnightMove( const pgn::Ply* ply, const bool isWhite );
 		~KnightMove();
-        /zdec *adasdasd 
-        * @brief 
-        *
-        * @param table
-        *
-        * @return 
-        */
 		bool  make( const Table* table ) const;
 	private:
-        /**
-        * @brief 
-        *
-        * @param table
-        * @param newPos
-        *
-        * @return 
-        */
-        /*adasdasd 
-        * @brief 
-        *
-        * @param table
-        * @param newPos
-        *
-        * @return 
-        */
 		pgn::Square try2Top1Right( const Table* table, const pgn::Square& newPos ) const;
 		pgn::Square try2Right1Top( const Table* table, const pgn::Square& newPos ) const;	
-        /zdec**adasdasd 
-        * @brief 
-        *
-        * @param table
-        * @param newPos
-        *
-        * @return 
-        */
-		pgn::Square try2Right1Bottom( const Table* table, const pgn::Square& newPos )<F6> const;
+		pgn::Square try2Right1Bottom( const Table* table, const pgn::Square& newPos ) const;
 		pgn::Square try2Bottom1Right( const Table* table, const pgn::Square& newPos ) const;
-        /*adasdasd 
-        * @brief 
-        *
-        * @param table
-        * @param newPos
-        *
-        * @return 
-        */
-		pgn::Square try2Bottom1Left( const Table* table, const pgn::Square& newPos ) const<F6>;
+		pgn::Square try2Bottom1Left( const Table* table, const pgn::Square& newPos ) const;
 		pgn::Square try2Left1Bottom( const Table* table, const pgn::Square& newPos ) const;
 		pgn::Square try2Left1Top( const Table* table, const pgn::Square& newPos ) const;
 		pgn::Square try2Top1Left( const Table* table, const pgn::Square& newPos ) const;
